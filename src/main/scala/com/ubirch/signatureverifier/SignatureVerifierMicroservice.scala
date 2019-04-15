538be9ac98b17f829a3392cf9fca4aaee8ed141c
mrobakowski <mikolaj.robakowski@ubirch.com>
2019-04-15 14:35:14 +0200

    fix signature-verifier errors

--- src/main/scala/com/ubirch/signatureverifier/SignatureVerifierMicroservice.scala
+++ src/main/scala/com/ubirch/signatureverifier/SignatureVerifierMicroservice.scala
@@ -1,22 +1,25 @@
 package com.ubirch.signatureverifier
 
+import java.security.SignatureException
+
 import com.ubirch.kafka.{MessageEnvelope, _}
 import com.ubirch.niomon.base.NioMicroservice
 import com.ubirch.niomon.base.NioMicroservice.WithHttpStatus
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerRecord
 
-import scala.util.{Failure, Try}
-
 class SignatureVerifierMicroservice(verifierFactory: NioMicroservice.Context => Verifier) extends NioMicroservice[MessageEnvelope, MessageEnvelope]("signature-verifier") {
   val verifier: Verifier = verifierFactory(context)
   override def processRecord(record: ConsumerRecord[String, MessageEnvelope]): ProducerRecord[String, MessageEnvelope] = {
-    Try {
+    // try ... catch is here, because `verifier.verify` may also throw
+    try {
       val pm = record.value().ubirchPacket
-      verifier.verify(pm.getUUID, pm.getSigned, 0, pm.getSigned.length, pm.getSignature)
-    }.filter(identity).recoverWith { case f: Throwable =>
-      Failure(WithHttpStatus(400, f))
-    }.get
+      if (!verifier.verify(pm.getUUID, pm.getSigned, 0, pm.getSigned.length, pm.getSignature)) {
+        throw new SignatureException("Invalid signature")
+      }
+    } catch { case e: Exception =>
+      throw WithHttpStatus(400, e)
+    }
 
     record.toProducerRecord(outputTopics("valid"))
   }
